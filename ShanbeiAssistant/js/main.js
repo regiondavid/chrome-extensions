a1de1f249a4ac6073a1f2da965312c57fa9091cf
regiondavid <uestczjw@gmail.com>
2016-11-08 20:47:39 +0800

    update rm the ads

--- ShanbeiAssistant/js/main.js
+++ ShanbeiAssistant/js/main.js
@@ -1,2 +1,42 @@
-let a = 3;
+var a = 3;
 console.log(a);
+var midAlert = document.getElementsByClassName('fsrFloatingContainer'),
+    topAd = document.getElementsByClassName('top-banner-ad-container'),
+    rightTab = document.getElementsByClassName('content__secondary-column'),
+    element = document.getElementsByClassName('element'),
+    topLeftLabel = document.getElementsByClassName('content__labels'),
+    contentFoot = document.getElementsByClassName('content-footer'),
+    lFooter = document.getElementsByClassName('l-footer'),
+    inlineAd = document.getElementsByClassName('ad-slot--inline'),
+    contentBody = document.getElementsByClassName('content__article-body'),
+    contentMeta = document.getElementsByClassName('content__meta-container');
+if(midAlert.length){
+    midAlert[0].style.display = "none";
+}
+if(topAd.length){
+    topAd[0].style.display = "none";
+}
+if(rightTab.length){
+    rightTab[0].style.display = "none";
+}
+if(element.length){
+    [].forEach.call(element,function(ele){
+        ele.setAttribute("style","opacity: 0");
+    });
+}
+if(topLeftLabel.length){
+    topLeftLabel[0].style.display = "none";
+}
+if(contentMeta.length){
+    contentMeta[0].style.display = "none";
+}
+if(contentFoot.length){
+    contentFoot[0].style.display = "none";
+}
+if(lFooter.length){
+    lFooter[0].style.display = "none";
+}
+if(inlineAd.length){
+    console.log(2333);
+    contentBody[0].removeChild(inlineAd[0]);
+}
---
+++ ShanbeiAssistant/js/removeAd.js
@@ -0,0 +1,5 @@
+document.getElementById('rmad').onclick = function(){
+    let i = 4;
+    console.log(i);
+    document.getElementsByClassName('sticky-top-banner-ad')[0].style.display = "none";
+};
